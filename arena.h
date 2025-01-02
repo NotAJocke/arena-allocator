da0e8099aab8ee3fcd18b1d46963464b3152546e
NotAJocke <jockerider199@gmail.com>
2025-01-02 14:32:23 +0100

    doc: add functions docstring

--- arena.h
+++ arena.h
@@ -10,11 +10,76 @@ typedef struct Arena {
   size_t capacity;
 } Arena;
 
+/**
+ * Creates a new memory arena with a specified initial capacity.
+ *
+ * @param initial_capacity The size of the memory region to allocate for the
+ * arena in bytes.
+ *
+ * @return A pointer to the newly created Arena structure, or NULL if allocation
+ * fails or if initial_capacity is 0.
+ */
 Arena *arena_create(size_t initial_capacity);
+
+/**
+ * Allocates a block of memory from the arena with the specified size and
+ * alignment.
+ *
+ * @param arena Pointer to the Arena from which memory will be allocated.
+ * @param size The size of the memory block to allocate in bytes.
+ * @param alignment The required memory alignment for the allocation.
+ *
+ * @return A pointer to the allocated memory block, or NULL if the allocation
+ * cannot be satisfied.
+ *
+ * @note Alignment ensures that the returned pointer adheres to specific memory
+ * alignment constraints.
+ */
 void *arena_alloc(Arena *arena, size_t size, size_t alignment);
+
+/**
+ * Resets the arena, making all memory available for allocation again without
+ * freeing the allocated memory block.
+ *
+ * @param arena Pointer to the Arena to clear.
+ *
+ * @note This operation does not free or reallocate the memory; it simply resets
+ * the allocation index.
+ */
 void arena_clear(Arena *arena);
+
+/**
+ * Frees the memory associated with the arena, including its memory region and
+ * the arena structure itself.
+ *
+ * @param arena Pointer to the Arena to destroy.
+ */
 void arena_destroy(Arena *arena);
+
+/**
+ * Expands the arena’s memory region to a new size if it exceeds the current
+ * capacity.
+ *
+ * @param arena Pointer to the Arena to expand.
+ * @param size The new size of the memory region in bytes.
+ *
+ * @return A pointer to the updated Arena, or NULL if the expansion fails or if
+ the requested size is less than or equal to the current capacity.
+ */
 Arena *arena_expand(Arena *arena, size_t size);
+
+/**
+ * Copies data from one arena to another.
+ *
+ * @param src Pointer to the source Arena.
+ * @param dest Pointer to the destination Arena.
+ *
+ * @return The number of bytes copied.
+ *
+ * @note If the destination arena’s capacity is smaller than the source arena’s
+ * current usage, only part of the data is copied.
+ * @note The destination arena’s index is updated to reflect the copied data.
+ */
 size_t arena_copy(Arena *src, Arena *dest);
 
 #endif // ARENA_H
